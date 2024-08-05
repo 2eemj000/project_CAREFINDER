2afe6edf2480beb1e71089edf7bc04c59aaf299e
flowerdeerjeannie <flowerdeer121212@gmail.com>
2024-08-05 14:06:56 +0900

    home 화면 작업

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
+import Home from './01/Home.js';
+import skyImage from './Img/Sky.jpg';
+import logoImage from './Img/Logo.png';
+import ButtonC from './UI/ButtonC.js';
 import './App.css';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    //   <RecoilRoot>
+    <MainApp />
+    //    </RecoilRoot>
+  );
+}
+
+function MainApp() {
+  return (
+    <div className="flex flex-col w-full max-w-screen-lg h-screen overflow-y-auto mx-auto">
+      <header className='flex justify-center items-center flex-col'>
+        <div className='logo-wrapper'>
+          <img src={logoImage} alt="Logo" className="logo-image" />
+        </div>
+        <div className='image-wrapper'>
+          <img src={skyImage} alt="Sky" className="background-image" />
+          <div className="text-overlay large-text">
+            오래 살수록 인생은 더 아름다워진다.
+          </div>
+          <div className="text-overlay medium-text">
+            "The longer I live, the more beautiful life becomes."
+          </div>
+          <div className="text-overlay small-text">
+            -Frank Lloyd Wright-
+          </div>
+        </div>
       </header>
+      <main className='flex-grow flex justify-center items-center'>
+        <ButtonC bcolor="teal" caption='SS' />
+      </main>
+      <footer className='flex justify-center items-center h-20 bg-slate-500 text-purple-50'>
+        ⓒ 2024 CAREFINDER, All rights reserved.
+      </footer>
     </div>
   );
 }
 
 export default App;
+
+//<Routes>
+//<Route path='/find' element={<Find />} />
+//<Route path='/health' element={<Health />} />
+//<Route path='/community' element={<Community />} />
+//<Route path='/qna' element={<Qna />} />
+//</Routes>
---
+++ src/UI/ButtonC.js
@@ -0,0 +1,34 @@
+import {hover} from "@testing-library/user-event/dist/hover"
+
+export default function ButtonC({caption, bcolor, handleClick}){
+     const colorB={
+          'orange' : 'bg-orange-500',
+          'red' : 'bg-red-500',
+          'green' : 'bg-green-300',
+          'lime' : 'bg-lime-500',
+          'sky' : 'bg-sky-500',
+          'violet' : 'bg-violet-400',
+          'teal' : 'bg-teal-400',
+
+     }
+
+     const colorBhover={
+          'orange' : 'hover:bg-orange-800',
+          'red' : 'hover:bg-red-800',
+          'green' : 'hover:bg-green-800',
+          'lime' : 'hover:bg-lime-800',
+          'sky' : 'hover:bg-sky-800',
+          'violet' : 'hover:bg-violet-800',
+          'teal' : 'hover:bg-teal-700',
+     }
+     
+     return (
+          <button className={`flex-shrink-0 justify-center items-center 
+               py-3 px-10 m-2
+                text-white font-bold rounded-md
+               ${colorB[bcolor]} ${colorBhover[bcolor]}`}
+               onClick={handleClick}>
+          {caption}
+          </button>
+     );
+}
