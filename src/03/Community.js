c62a40cae406af43590af88ab932e8f441b1b2d4
2eemj000 <2eemj000@gmail.com>
2024-08-06 17:03:28 +0900

    community 추가

--- src/03/Community.js
+++ src/03/Community.js
@@ -1,9 +1,35 @@
-import React from 'react'
+import React from 'react';
+import './community.css';
 
-export default function Community() {
+function Community() {
+  const data = Array.from({ length: 10 }, (_, index) => ({
+    번호: index + 1,
+    제목: `제목 ${index + 1}`,
+    작성자: `작성자 ${index + 1}`,
+  }));
+
   return (
-    <div>
-      Community
+    <div className="container">
+      <table>
+        <thead>
+          <tr>
+            <th>번호</th>
+            <th>제목</th>
+            <th>작성자</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data.map((item) => (
+            <tr key={item.번호}>
+              <td>{item.번호}</td>
+              <td>{item.제목}</td>
+              <td>{item.작성자}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
-  )
+  );
 }
+
+export default Community;
