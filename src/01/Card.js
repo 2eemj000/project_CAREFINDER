f5bd66db7c8a9882783ddbb598e42076b52a1b61
flowerdeerjeannie <flowerdeer121212@gmail.com>
2024-08-12 13:08:33 +0900

    props 수정

--- src/01/Card.js
+++ src/01/Card.js
@@ -1,27 +1,31 @@
+import React from 'react';
+import PropTypes from 'prop-types';
 
-//TODO : props 제대로 받도록 정리
-
-export default function Card({ title, content, onClick, level }) {
-     return (
-          <div
-          onClick={onClick}
-          className="cursor-pointer max-w-sm p-4 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100"
-      >
-          <h5 className="text-lg font-semibold mb-2">{title}</h5>
-          <p className="text-sm text-gray-700">{content}</p>
-          <div className="px-2 pt-2 pb-4 flex flex-wrap">
-                {level.map((level, index) => (
+export default function Card({ name, phone, onClick, level }) {
+    return (
+        <div
+            onClick={onClick}
+            className="cursor-pointer max-w-sm p-4 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100"
+        >
+            <h5 className="text-lg font-semibold mb-2">{name}</h5>
+            <p className="text-sm text-gray-700">{phone}</p>
+            <div className="px-2 pt-2 pb-4 flex flex-wrap">
+                {level.map((lvl, index) => (
                     <span
                         key={index}
                         className="inline-block bg-gray-200 rounded-full px-2 py-1 text-xs font-semibold text-gray-700 mr-1 mb-1"
                     >
-                        {level}
+                        {lvl}
                     </span>
                 ))}
             </div>
-      </div>
-
-     )
+        </div>
+    );
 }
 
-
+Card.propTypes = {
+    name: PropTypes.string.isRequired,
+    phone: PropTypes.string.isRequired,
+    onClick: PropTypes.func.isRequired,
+    level: PropTypes.arrayOf(PropTypes.string)
+};
--- src/01/CardDetail.js
+++ src/01/CardDetail.js
@@ -5,7 +5,6 @@ export default function CardDetail() {
     const { cardId } = useParams();
     const [cardDetails, setCardDetails] = useState(null);
 
-    
     useEffect(() => {
         async function fetchCardDetails() {
             try {
@@ -24,23 +23,25 @@ export default function CardDetail() {
         if (cardDetails && cardDetails.locx && cardDetails.locy) {
             const { kakao } = window; // Kakao Map API 객체
 
-            // 지도 컨테이너
-            const mapContainer = document.getElementById('map');
-            // 지도 옵션
-            const mapOption = {
-                center: new kakao.maps.LatLng(cardDetails.locy, cardDetails.locx), // 위도, 경도
-                level: 3 // 지도의 확대 레벨
-            };
+            if (kakao && kakao.maps) {
+                // 지도 컨테이너
+                const mapContainer = document.getElementById('map');
+                // 지도 옵션
+                const mapOption = {
+                    center: new kakao.maps.LatLng(cardDetails.locy, cardDetails.locx), // 위도, 경도
+                    level: 3 // 지도의 확대 레벨
+                };
 
-            // 지도 생성
-            const map = new kakao.maps.Map(mapContainer, mapOption);
+                // 지도 생성
+                const map = new kakao.maps.Map(mapContainer, mapOption);
 
-            // 마커 생성
-            const markerPosition = new kakao.maps.LatLng(cardDetails.locy, cardDetails.locx); // 위도, 경도
-            const marker = new kakao.maps.Marker({
-                position: markerPosition
-            });
-            marker.setMap(map);
+                // 마커 생성
+                const markerPosition = new kakao.maps.LatLng(cardDetails.locy, cardDetails.locx); // 위도, 경도
+                const marker = new kakao.maps.Marker({
+                    position: markerPosition
+                });
+                marker.setMap(map);
+            }
         }
     }, [cardDetails]);
 
