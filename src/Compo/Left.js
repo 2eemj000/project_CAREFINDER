b62a61913b0f2ea7fa3ceb18fcae34e1f93a4972
flowerdeerjeannie <flowerdeer121212@gmail.com>
2024-08-20 11:28:20 +0900

    찜하기 기능 추가

--- src/Compo/Left.js
+++ src/Compo/Left.js
@@ -102,41 +102,6 @@ function NavigationBar() {
         </li>
         {/* 로그인 및 회원가입 버튼 추가 */}
         <div className="auth-buttons">
-<<<<<<<<< Temporary merge branch 1
-  {!isLoggedIn ? (
-    <>
-      <button
-        className="sign-button"
-        onClick={() => setShowLoginModal(true)}
-      >
-        로그인
-      </button>
-      <button
-        className="sign-button"
-        onClick={() => setShowSignup(true)}
-      >
-        회원가입
-      </button>
-    </>
-  ) : (
-    <>
-      <p2 className="welcome-message">{loginMessage}</p2>
-      <button
-        className="sign-button"
-        onClick={handleMyPageClick}
-      >
-        마이페이지
-      </button>
-      <button
-        className="sign-button"
-        onClick={handleLogout}
-      >
-        로그아웃
-      </button>
-    </>
-  )}
-</div>
-=========
           {!isLoggedIn ? (
             <>
               <button
@@ -170,7 +135,6 @@ function NavigationBar() {
             </>
           )}
         </div>
->>>>>>>>> Temporary merge branch 2
       </ul>
 
       {showLoginModal && <LoginForm onClose={() => setShowLoginModal(false)} onLoginSuccess={handleLoginSuccess} />}
